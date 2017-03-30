de08d2db227535b43627732062f16ba7e8a21cbd
林金星 <linjinxing493@pingan.com.cn>
2017-03-30 10:00:40 +0800

    init

---
+++ Crash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crash
+//
+//  Created by 林金星 on 17/3/30.
+//  Copyright © 2017年 Pingan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Crash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crash
+//
+//  Created by 林金星 on 17/3/30.
+//  Copyright © 2017年 Pingan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
